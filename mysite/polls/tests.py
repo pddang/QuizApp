495a7b8190b05f15f0213d48c951a54b29599517
Phan Dang <philip.ddang>
2020-04-07 22:13:36 -0700

    first commit

---
+++ mysite/polls/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+# Create your tests here.
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
